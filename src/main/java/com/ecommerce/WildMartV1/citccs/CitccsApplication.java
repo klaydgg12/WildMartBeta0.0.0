d76b2475afd88791935b87ce1a9848adb4a80a38
Etherealite769 <cubiangd93@gmail.com>
2025-11-11 14:51:08 +0800

    hehe java bleeeh

---
+++ src/main/java/com/ecommerce/WildMartV1/citccs/CitccsApplication.java
@@ -0,0 +1,13 @@
+package com.ecommerce.WildMartV1.citccs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CitccsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CitccsApplication.class, args);
+	}
+
+}
