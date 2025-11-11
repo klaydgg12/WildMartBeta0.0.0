9b476beb87f4da20d07b87881b8593312ba87cb5
Etherealite769 <cubiangd93@gmail.com>
2025-11-11 14:47:55 +0800

    setup dto

---
+++ src/main/java/com/ecommerce/WildMartV1/citccs/dto/UserDTO.java
@@ -0,0 +1,125 @@
+package com.ecommerce.WildMartV1.citccs.dto;
+
+public class UserDTO {
+    private Long id;
+    private String username;
+    private String email;
+    private String phone;
+    private String address;
+    private String city;
+    private String zipCode;
+    private String country;
+    private String bio;
+    private String profileImageUrl;
+    private Double rating;
+    private Integer productCount;
+    private Integer salesCount;
+    
+    // Constructors
+    public UserDTO() {}
+    
+    // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getUsername() {
+        return username;
+    }
+    
+    public void setUsername(String username) {
+        this.username = username;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    public String getPhone() {
+        return phone;
+    }
+    
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    
+    public String getAddress() {
+        return address;
+    }
+    
+    public void setAddress(String address) {
+        this.address = address;
+    }
+    
+    public String getCity() {
+        return city;
+    }
+    
+    public void setCity(String city) {
+        this.city = city;
+    }
+    
+    public String getZipCode() {
+        return zipCode;
+    }
+    
+    public void setZipCode(String zipCode) {
+        this.zipCode = zipCode;
+    }
+    
+    public String getCountry() {
+        return country;
+    }
+    
+    public void setCountry(String country) {
+        this.country = country;
+    }
+    
+    public String getBio() {
+        return bio;
+    }
+    
+    public void setBio(String bio) {
+        this.bio = bio;
+    }
+    
+    public String getProfileImageUrl() {
+        return profileImageUrl;
+    }
+    
+    public void setProfileImageUrl(String profileImageUrl) {
+        this.profileImageUrl = profileImageUrl;
+    }
+    
+    public Double getRating() {
+        return rating;
+    }
+    
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+    
+    public Integer getProductCount() {
+        return productCount;
+    }
+    
+    public void setProductCount(Integer productCount) {
+        this.productCount = productCount;
+    }
+    
+    public Integer getSalesCount() {
+        return salesCount;
+    }
+    
+    public void setSalesCount(Integer salesCount) {
+        this.salesCount = salesCount;
+    }
+}
